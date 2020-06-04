793ab4ea661d31af125916664ea3c6c65ce7f394
Farzad FARID <farzad.farid@kapten.com>
2020-06-04 16:26:26 +0200

    Improve nested lists

--- python/nested-lists.py
+++ python/nested-lists.py
@@ -1,13 +1,17 @@
 if __name__ == '__main__':
     arr = []
     min_score = float("inf")
+    second_min_score = float("inf")
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        min_score = min(min_score, score)
+        if score < min_score:
+            second_min_score = min_score
+            min_score = score
+        elif min_score < score < second_min_score:
+            second_min_score = score
         arr.append([name, score])
 
-    second_min_score = sorted([el[1] for el in arr if el[1] != min_score])[0]
     names = [el[0] for el in arr if el[1] == second_min_score]
 
     for name in sorted(names):
