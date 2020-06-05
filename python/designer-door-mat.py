f2ce271cb32c6a544a931b70d283f288ed34acd9
Farzad FARID <farzad.farid@kapten.com>
2020-06-05 14:59:38 +0200

    Improve Designer door mat

--- python/designer-door-mat.py
+++ python/designer-door-mat.py
@@ -3,7 +3,7 @@ PATTERN = ".|."
 N, M = map(int, input().split())
 
 for row in range(int((N-1) / 2)):
-    print(("-" * int(M/2 - 3*row - 1)) + (PATTERN * (2 * row + 1)) + ("-" * int(M/2 - 3*row - 1)))
-print(("-" * int((M-7)/2)) + "WELCOME" + ("-" * int((M-7)/2)))
+    print((PATTERN * (2 * row + 1)).center(M, '-'))
+print("WELCOME".center(M, "-"))
 for row in range(int((N-1) / 2) -1, -1, -1):
-    print(("-" * int(M/2 - 3*row - 1)) + (PATTERN * (2 * row + 1)) + ("-" * int(M/2 - 3*row - 1)))
+    print((PATTERN * (2 * row + 1)).center(M, '-'))
