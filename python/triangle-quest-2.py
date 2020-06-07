09d61839866a567e53ba882620cde271859cc308
Farzad FARID <farzy@farzy.org>
2020-06-07 09:38:41 +0200

    Simplify Triangle Quest 2

--- python/triangle-quest-2.py
+++ python/triangle-quest-2.py
@@ -1,2 +1,3 @@
 for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
-    print((123456789//(10**(9-i)) * 10**(i-1) + 87654321 - (87654321 // 10**(i-1) * 10**(i-1))) if i !=1 else 1)
+    print(123456789//(10**(9-i)) * 10**(i-1) +
+          87654321 - (87654321 // 10**(i-1) * 10**(i-1)))
