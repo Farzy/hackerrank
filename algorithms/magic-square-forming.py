595a7bf47fd6862b5c61c8d634c8047ec437544e
Farzad FARID <farzy@farzy.org>
2020-06-13 18:49:13 +0200

    magic-square-forming: Improve readability

--- algorithms/magic-square-forming.py
+++ algorithms/magic-square-forming.py
@@ -1,23 +1,17 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
 import itertools
 
 
-def is_magic_square(s):
-    # print("Is {} magic?".format(s))
-    s1 = sum(s[0:3])
-    s2 = sum(s[3:6])
-    s3 = sum(s[6:9])
-    s4 = sum(s[i] for i in [0, 3, 6])
-    s5 = sum(s[i] for i in [1, 4, 7])
-    s6 = sum(s[i] for i in [2, 5, 8])
-    s7 = sum(s[i] for i in [0, 4, 8])
-    s8 = sum(s[i] for i in [2, 4, 6])
+def is_magic_square(square):
+    s1 = sum(square[0:3])
+    s2 = sum(square[3:6])
+    s3 = sum(square[6:9])
+    s4 = sum(square[0::3])
+    s5 = sum(square[1::3])
+    s6 = sum(square[2::3])
+    s7 = sum(square[0::4])
+    s8 = sum(square[2:7:2])
 
     return s1 == s2 == s3 == s4 == s5 == s6 == s7 == s8
 
@@ -31,9 +25,9 @@ def diff_squares(square_a, square_b):
 
 
 # Complete the formingMagicSquare function below.
-def formingMagicSquare(s):
+def forming_magic_square(square):
     min_sum = 999
-    flat_s = tuple(x for row in s for x in row)
+    flat_s = tuple(x for row in square for x in row)
 
     for square in create_magic_squares_iter():
         min_sum = min(min_sum, diff_squares(flat_s, square))
@@ -49,7 +43,7 @@ if __name__ == '__main__':
     for _ in range(3):
         s.append(list(map(int, input().rstrip().split())))
 
-    result = formingMagicSquare(s)
+    result = forming_magic_square(s)
 
     # fptr.write(str(result) + '\n')
 
