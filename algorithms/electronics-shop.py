24bc4f8b27d2b435468f4bb7b942973800eefdf1
Farzad FARID <farzy@farzy.org>
2020-06-13 16:24:42 +0200

    electronics-shop: Add

---
+++ algorithms/electronics-shop.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    # List of all price combinations less than or equal to b
+    sums = [k+d for k in keyboards for d in drives if k + d <= b]
+    sums.sort()
+    if len(sums) == 0:
+        return -1
+    else:
+        return sums[-1]
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    # fptr.write(str(moneySpent) + '\n')
+
+    # fptr.close()
+
+    print(moneySpent)
