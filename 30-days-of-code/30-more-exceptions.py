89f1698f174f91cef7731f8c8fdd48264e64d269
Farzad FARID <farzy@farzy.org>
2020-06-20 11:26:00 +0200

    30-more-exceptions: Add

---
+++ 30-days-of-code/30-more-exceptions.py
@@ -0,0 +1,18 @@
+class Calculator(object):
+    @staticmethod
+    def power(n, p):
+        if n >= 0 and p >= 0:
+            return n ** p
+        else:
+            raise Exception("n and p should be non-negative")
+
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
