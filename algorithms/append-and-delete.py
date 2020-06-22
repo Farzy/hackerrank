b355c53d6e90556e65d1da08f1ed89057741e2de
Farzad FARID <farzad.farid@kapten.com>
2020-06-22 18:59:38 +0200

    append-and-delete: Working solution at last

--- algorithms/append-and-delete.py
+++ algorithms/append-and-delete.py
@@ -1,22 +1,21 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
 
 # Complete the appendAndDelete function below.
 def appendAndDelete(s, t, k):
-    n = 0
-    len_s = len(s)
-    len_t = len(t)
-    while n < min(len_s, len_t) and s[n] == t[n]:
-        n += 1
-    if k > (len(t) - n) and len(s) + len(t) - 2 * n <= k:
+    common_len = 0
+    s_total = len(s)
+    t_total = len(t)
+    while common_len < min(s_total, t_total) and \
+            s[common_len] == t[common_len]:
+        common_len += 1
+    extra_delete_moves = k - s_total - t_total + 2 * common_len
+    if (0 <= extra_delete_moves <= common_len * 2 and extra_delete_moves % 2 == 0) or \
+            extra_delete_moves > common_len * 2:
         return "Yes"
     else:
         return "No"
+
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
