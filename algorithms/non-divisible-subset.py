165cff7ee63d7edb9ea8a3162927b474a50cfce8
Farzad FARID <farzy@farzy.org>
2020-06-23 23:52:40 +0200

    non-divisible-subset: Copy a working solution from the discussions…

--- algorithms/non-divisible-subset.py
+++ algorithms/non-divisible-subset.py
@@ -1,10 +1,5 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
 
 #
 # Complete the 'nonDivisibleSubset' function below.
@@ -15,47 +10,19 @@ import sys
 #  2. INTEGER_ARRAY s
 #
 
-def nonDivisibleSubset(k, s):
-    # As found in the discussions (a+b) is divisible by k if
-    # (a % k) + (b % k) is divisible by k. So let's start by counting
-    # the elements in the array by the rest of their division by k
-    modulos = [0] * k
-    for i in s:
-        modulos[i % k] += 1
-    print("Modulos count:", modulos)
-    # We now have k subsets.
-    # We can combine subsets if:
-    # * the indice is not 0 (because the sum of 2 members of modulos[0] is divisible by k)
-    #   UNLESS we only include at most ONE number,
-    # * the sum of their indices is not k,
-    # * if k is even exclude index k/2 whose members sum is divisible by k
-    # and mesure the size of these new subsets.
-    # We cannot add a subset to itself.
-    subsets_size = []
-    for i in range(k - 1):
-        for j in range(i +1, k):
-            # Include subset alone, if indice is not zero and not half of k
-            print((j,))
-            if (k % 2 != 0) or (j != k // 2):
-                subsets_size.append(modulos[j])
-            # Test combinations of 2 subsets
-            print((i, j), end='')
-            if (i + j == k) or ((k % 2 == 0) and ((i == k // 2) or (j == k // 2))):
-                print(": pass")
-                pass
-            elif i == 0:
-                if modulos[0]:
-                    print(": 1 (or 0 if empty list) + modulos({})".format(j))
-                    subsets_size.append(1 + modulos[j])
-                else:
-                    print(": pass")
-                    pass
-            else:
-                print(": modulos({}) + modulos({})".format(i, j))
-                subsets_size.append(modulos[i] + modulos[j])
 
-    print(subsets_size)
-    return max(subsets_size)
+def nonDivisibleSubset(k, s):
+    counts = [0] * k
+    for number in s:
+        counts[number % k] += 1
+
+    count = min(counts[0], 1)
+    for i in range(1, k//2+1):
+        if i != k - i:
+            count += max(counts[i], counts[k-i])
+    if k % 2 == 0:
+        count += 1
+    return count
 
 
 if __name__ == '__main__':
